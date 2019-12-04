847cc8b1994433c38af4b9cfa877a47b6a1aa71d
ttaiken <zjqisme@126.com>
2019-12-04 17:23:16 +0900

    SCSS bootstrap Theam

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DropdownComponent } from './UI/Menu/dropdown/dropdown.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent
   ],
   imports: [
     BrowserModule,
